1b4da3f75740b0b7634fe36bb8897240dbdd4226
Walle908 <walle0908@yandex.ru>
2026-05-04 18:25:58 +0400

    refactor: change favicon and base in vite.config

--- rs-react-app/vite.config.ts
+++ rs-react-app/vite.config.ts
@@ -2,10 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/React_2026_Q2-Walle908/',
-  server: {
-    open: true,
-  },
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react()],
+    base: command === 'build' ? '/React_2026_Q2-Walle908/' : '/',
+    server: {
+      open: true,
+    },
+  };
 });
