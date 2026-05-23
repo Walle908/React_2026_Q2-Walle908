c0b271ba8f7acfe2cc07cd73abff2f688f6f0e1e
Walle908 <walle0908@yandex.ru>
2026-05-24 00:37:46 +0400

    refactor: simplify ResultBlock

--- rs-react-app/src/components/ResultBlock/ResultBlock.test.tsx
+++ rs-react-app/src/components/ResultBlock/ResultBlock.test.tsx
@@ -6,7 +6,7 @@ import { ErrorMessage } from '../../constants/constants';
 import { mockCharacters } from '../../__tests__/mocks';
 
 describe('ResultBlock Component', () => {
-  it('should render a list of Card components when chars array is not empty', () => {
+  it('should render a list of Card components when when errorMessage is NO_ERROR', () => {
     render(
       <MemoryRouter>
         <ResultBlock chars={mockCharacters} errorMessage={ErrorMessage.NO_ERROR} />
@@ -20,38 +20,36 @@ describe('ResultBlock Component', () => {
       screen.getByRole('heading', { level: 2, name: mockCharacters[1]?.name })
     ).toBeInTheDocument();
 
-    const errorHeading = screen.queryByRole('heading', {
-      level: 2,
-      name: new RegExp(ErrorMessage.NOT_FOUND),
-    });
-    expect(errorHeading).not.toBeInTheDocument();
+    expect(screen.queryByTestId('bad-result')).not.toBeInTheDocument();
   });
+});
 
-  it('should render an error message when chars array is empty and errorMessage is provided', () => {
-    render(
-      <MemoryRouter>
-        <ResultBlock chars={[]} errorMessage={ErrorMessage.NOT_FOUND} />
-      </MemoryRouter>
-    );
+it('should render an error message when errorMessage is NOT_FOUND', () => {
+  render(
+    <MemoryRouter>
+      <ResultBlock chars={[]} errorMessage={ErrorMessage.NOT_FOUND} />
+    </MemoryRouter>
+  );
 
-    const errorHeading = screen.getByTestId('bad-result');
+  const errorHeading = screen.getByTestId('bad-result');
 
-    expect(errorHeading).toBeInTheDocument();
-    expect(errorHeading.textContent).toBe(ErrorMessage.NOT_FOUND);
+  expect(errorHeading).toBeInTheDocument();
+  expect(errorHeading.textContent).toBe(ErrorMessage.NOT_FOUND);
 
-    expect(
-      screen.queryByRole('heading', { level: 2, name: 'Rick Sanchez' })
-    ).not.toBeInTheDocument();
-  });
+  expect(
+    screen.queryByRole('heading', { level: 2, name: mockCharacters[0]?.name })
+  ).not.toBeInTheDocument();
+});
 
-  it('should render initial empty state and no error messages before any search', () => {
-    render(
-      <MemoryRouter>
-        <ResultBlock chars={[]} errorMessage={ErrorMessage.NO_ERROR} />
-      </MemoryRouter>
-    );
+it('should render an error message when errorMessage is ANOTHER_ERROR', () => {
+  render(
+    <MemoryRouter>
+      <ResultBlock chars={[]} errorMessage={ErrorMessage.ANOTHER_ERROR} />
+    </MemoryRouter>
+  );
 
-    const errorHeading = screen.queryByRole('heading', { level: 2 });
-    expect(errorHeading).not.toBeInTheDocument();
-  });
+  const errorHeading = screen.getByTestId('bad-result');
+
+  expect(errorHeading).toBeInTheDocument();
+  expect(errorHeading.textContent).toBe(ErrorMessage.ANOTHER_ERROR);
 });
--- rs-react-app/src/components/ResultBlock/ResultBlock.tsx
+++ rs-react-app/src/components/ResultBlock/ResultBlock.tsx
@@ -11,16 +11,22 @@ interface ResultSectionProps {
 }
 
 export default function ResultBlock({ chars, errorMessage }: ResultSectionProps): ReactNode {
+  if (errorMessage !== ErrorMessage.NO_ERROR) {
+    return (
+      <div className={styles.resultBlock}>
+        <Text as="h2" className={styles.badResult} data-testid="bad-result">
+          {errorMessage}
+        </Text>
+      </div>
+    );
+  }
+
   return (
     <div className={styles.resultBlock}>
       <div className={styles.cardsWrapper}>
-        {chars.length > 0
-          ? chars.map((char) => <Card key={char.id} char={char} />)
-          : errorMessage !== ErrorMessage.NO_ERROR && (
-              <Text as="h2" className={styles.badResult} data-testid="bad-result">
-                {errorMessage}
-              </Text>
-            )}
+        {chars.map((char) => (
+          <Card key={char.id} char={char} />
+        ))}
       </div>
     </div>
   );
