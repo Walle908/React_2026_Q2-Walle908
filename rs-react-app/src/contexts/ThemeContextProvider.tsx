340ca7ee93bda97c9e2577b1bd0088904e492e16
Walle908 <walle0908@yandex.ru>
2026-05-31 20:44:28 +0400

    refactor: ThemeContextProvider and styles

--- rs-react-app/src/contexts/ThemeContextProvider.tsx
+++ rs-react-app/src/contexts/ThemeContextProvider.tsx
@@ -6,7 +6,7 @@ import styles from './ThemeContextProvider.module.css';
 const DARK_THEME_CLASS = 'dark';
 const THEME_LS_KEY = 'theme_walle908';
 
-export function ThemeProvider({ children }: { children: ReactNode }) {
+export function ThemeProvider({ children }: { children: ReactNode }): ReactNode {
   const [theme, setTheme] = useLocalStorage<string>(THEME_LS_KEY, 'light');
 
   const isDarkTheme = theme === 'dark';
@@ -25,7 +25,7 @@ export function ThemeProvider({ children }: { children: ReactNode }) {
 
   return (
     <ThemeContext.Provider value={{ isDarkTheme, toggleTheme }}>
-      <div className={`${styles.container} ${isDarkTheme ? DARK_THEME_CLASS : ''}`}>{children}</div>
+      <div className={styles.container}>{children}</div>
     </ThemeContext.Provider>
   );
 }
